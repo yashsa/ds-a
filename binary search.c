6f2737f792addc39f12bbab56393c0e91c2838e3
Yash Saxena <ysaxena52@gmail.com>
2018-08-13 04:53:25 +0530

    binary search with improved readability

--- binary search.c
+++ binary search.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
+
+/* searches for a given 'key'(x) in a sorted   */
 int binary_search(int a[],int x, int left, int right)
 {
-    if(left>right){return 0;}
+    if(left>right)
+  	return 0;
     int mid= (left+right)/2;
-    if(x==a[mid]){return 1;}
-    else if(x<a[mid]){return binary_search(a,x,left,mid-1);}
-    else {return binary_search(a,x,mid+1,right);}
+
+    if(x==a[mid]) 
+	return 1;
+
+    else if(x<a[mid]) 
+	return binary_search(a,x,left,mid-1);
+
+    else 
+	return binary_search(a,x,mid+1,right);
+
 }
 int main() {
 	int n;
 	int x;
 	scanf("%d\n%d",&n,&x);
-//	scanf("%d",&x);
 	int a[n];
 	
 	for(int i=0; i<n; i++)
@@ -21,6 +30,7 @@ int main() {
 	
 	int ans=binary_search(a,x,0,n-1);
 	printf("%d",ans);
+
 	return 0;
 }
 
