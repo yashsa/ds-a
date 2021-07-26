e2273ae2e56fb007273b4e9efdf07d8f5dd4af8d
yashsa <ysaxena52@gmail.com>
2021-07-26 20:24:21 +0530

    Create Append Numbers to List to Create Range.c

---
+++ Append Numbers to List to Create Range.c
@@ -0,0 +1,21 @@
+int solve(vector<int>& nums, int k) {
+    sort(nums.begin(),nums.end());
+    int count=0;
+    int maxSumAchievableTillNow=0;
+    int i=0;
+    while(i<nums.size()){
+        if(maxSumAchievableTillNow>=k) return count;
+        else if(nums[i]>maxSumAchievableTillNow+1){
+            count++;
+            maxSumAchievableTillNow=2*maxSumAchievableTillNow+1;
+            continue;
+        }
+        maxSumAchievableTillNow+=nums[i];
+        i++;
+    }
+    while(maxSumAchievableTillNow<k){
+        count++;
+        maxSumAchievableTillNow=2*maxSumAchievableTillNow+1;
+    }
+    return count;
+}
